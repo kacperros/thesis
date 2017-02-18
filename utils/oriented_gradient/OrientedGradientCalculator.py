b861b11fd00fb82c59f7b1f15a50a80c27c25277
Kacper Roszczyna <kacper.ros94@gmail.com>
2017-02-18 13:02:15 +0100

    oriented gradient calculation

---
+++ utils/oriented_gradient/OrientedGradientCalculator.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from utils.RotationAdapter.RotationAdapter import RotationAdapter
+
+
+class OrientedGradientCalculator(RotationAdapter):
+    def __init__(self, img, radius, angle):
+        RotationAdapter.__init__(self, img)
+        self.radius = radius - 1
+        self.angle = angle
+
+    def _operate(self):
+        output = np.zeros((self._rotated.shape[0], self._rotated.shape[1]))
+        rows, cols = self._rotated.shape[0], self._rotated.shape[1]
+        for i in range(self.radius, rows - self.radius + 1):
+            for j in range(self.radius, cols - self.radius + 1):
+                output[i, j] = self._calculate_single_oriented_gradient(
+                    self._rotated[i - self.radius:i + self.radius, j - self.radius:j + self.radius])
+        output = np.around(output, 3)
+        self._rotated = output
+
+    def calculate(self):
+        return self.adapt(self.angle)
+
+    def _calculate_single_oriented_gradient(self, img_part):
+        top, dists = np.histogram(img_part[0:self.radius, :], 16, (0, 255))
+        bottom, dists2 = np.histogram(img_part[self.radius:, :], 16, (0, 255))
+        return 0.5 * np.sum(np.nan_to_num(np.divide(np.subtract(top, bottom) ** 2, np.add(top, bottom))))
--- utils/oriented_gradient/tests/testOG.py
+++ utils/oriented_gradient/tests/testOG.py
@@ -1,27 +1,31 @@
 from unittest import TestCase
+
 import cv2
-import utils.oriented_gradient.oriented_gradient as og
 import numpy as np
+
+from utils.oriented_gradient.OrientedGradientCalculator import OrientedGradientCalculator
 
 
 class TestOG(TestCase):
-    def test_calculate_single_oriented_gradient(self):
-        img = cv2.imread('gaussian_test_img.png')
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-        img = img[:, :, 0]
-        distance = og.calculate_single_oriented_gradient(img[1:5, 1:5], 2)
-        self.assertAlmostEqual(np.around(distance, 2), 2.67, 2)
+    def setUp(self):
+        self.img = cv2.imread('gaussian_test_img.png')
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB)
+        self.img = self.img[:, :, 0]
 
-    def test_calculate_oriented_gradient1(self):
-        img = cv2.imread('gaussian_test_img.png')
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-        img = img[:, :, 0]
-        distances = og.calculate_oriented_gradient(img, 0, 6)
-        self.assertEqual(distances[5, 5], 0)
+    def test_calculate_oriented_gradient(self):
+        object_under_test = OrientedGradientCalculator(self.img, 6, 0)
+        result = object_under_test.calculate()
+        np.testing.assert_array_almost_equal(np.full((result.shape[0], result.shape[1]), 0.), result)
+        print("Done")
 
     def test_calculate_oriented_gradient2(self):
-        img = cv2.imread('gaussian_test_img.png')
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-        img = img[:, :, 0]
-        distances = og.calculate_oriented_gradient(img, 60, 6)
-        self.assertEqual(distances[5, 5], 0)
+        object_under_test = OrientedGradientCalculator(self.img, 2, 0)
+        result = object_under_test.calculate()
+        result = np.around(result, 2)
+        np.testing.assert_array_equal(result[3:8, 3:8],
+                                      np.array([[0.67, 2., 2., 2., 0.67],
+                                                [0., 0., 0., 0., 0.],
+                                                [0., 0., 0., 0., 0.],
+                                                [0., 0., 0., 0., 0.],
+                                                [0.67, 2., 2., 2., 0.67]]))
+        print("Done")
